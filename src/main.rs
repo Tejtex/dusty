a77d4f910bbaa6a6414aecad3ecf5d6df8b601d3
Tejtex <raxeli1.mc@gmail.com>
2025-10-01 19:53:15 +0200

    df

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
+use std::time::Duration;
+use color_eyre::Result;
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{DefaultTerminal, Frame};
+
 mod app;
 
 mod dirscreen;
 mod filescreen;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if event::poll(Duration::from_millis(250))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Char(c) => {
+                        println!("Pressed key: {}", c);
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
